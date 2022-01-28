5bac1ab2d06de0d28340e0b8aef7adb0142dba3d
David Paredes <dparedes@carpages.ca>
2022-01-27 23:33:43 -0500

    lap time rework

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,8 @@ let mins = '00';
 let secs = '00';
 let tenths = '00';
 let tenthInterval;
-let lapTimesClicked = [];
-let lapTimesConverted = [];
+let prevLap = {mins: '00', secs: '00', tenths: '00' };
+let lapTimes = [];
 
 // hide or show buttons when clicking start or stop
 function toggleHiddenUnhidden() {
@@ -20,7 +20,8 @@ function toggleHiddenUnhidden() {
 // start the interval
 function start() {
   toggleHiddenUnhidden();
-  tenthInterval = setInterval(timer, 10);
+  // tenthInterval = setInterval(timer, 10);
+  tenthInterval = setInterval(timer, 1);
 }
 
 function timer() {
@@ -51,6 +52,13 @@ function timer() {
     secSpan.innerHTML = secs;
     mins++;
     minSpan.innerHTML = `0${mins}`;
+
+    if (mins <= 9) {
+      mins = `0${mins}`;
+      minSpan.innerHTMl = mins;
+    } else {
+      minSpan.innerHTML = mins;
+    }
   }
 
   // reset and stop when timer reaches 59 mins, 59 seconds and 99 tenths
@@ -62,38 +70,75 @@ function timer() {
 }
 
 function lap() {
-  // lap click sets current mins secs and tenths to lapTime
-  let lapTime = `${mins}${secs}.${tenths}`;
-console.log('mins: ', mins, 'secs: ', secs, 'tenths: ', tenths)
-console.log('lapBtn click lapTime: ', lapTime);
-  // lapTime is added to the beginning of lapTimesClicked array
-  lapTimesClicked.unshift(lapTime);
-console.log('lapTimesClicked array: ', lapTimesClicked);
-  
-  // if lapTimesClicked has a second entry, 
-  // that second entry is subtracted from the current lapTime
-  if (lapTimesClicked[1]) {
-    let prevLapTime = lapTimesClicked[1];
-    lapTimeConverted = (lapTime - prevLapTime).toFixed(2);
+  let lapMins = mins - prevLap.mins;
+  let lapSecs = secs - prevLap.secs;
+  let lapTenths = tenths - prevLap.tenths;
 
-    lapTimesConverted.unshift(lapTimeConverted);
-  } else {
-    // if this is the first time lap is clicked
-    // the current lap time is pushed into lapTimesConverted
-    // parseFloat so it doesn't show with leading zeroes
-    lapTimesConverted.push(parseFloat(lapTime));
+  if (lapMins < 0) {
+    lapMins = lapMins + 60;
   }
 
-  lapInfo.innerHTML = lapTimesConverted.map(lapTime => {
-    // fastest lap, longest lap, neither
-    if( lapTime == Math.min(...lapTimesConverted)) {
-      return `<li class="green white-text">${lapTime}</li>`;
-    } else if (lapTime == Math.max(...lapTimesConverted))  {
-      return `<li class="red white-text">${lapTime}</li>`;
-    } else {
-      return `<li>${lapTime}</li>`;
-    } 
+  if (lapMins <= 9) {
+    lapMins = `0${lapMins}`;
+  }
+
+  if (lapSecs < 0) {
+    lapSecs = lapSecs + 60;
+  }
+
+  if (lapSecs <= 9) {
+    lapSecs = `0${lapSecs}`;
+  }
+
+  if (lapTenths < 0) {
+    lapTenths = lapTenths + 100;
+  }
+
+  if (lapTenths < 9) {
+    lapTenths = `0${lapTenths}`;
+  }
+
+  prevLap = {
+    mins,
+    secs,
+    tenths
+  }
+
+  lapTimes.unshift(`${lapMins}:${lapSecs}.${lapTenths}`);
+
+  console.log('converted lap* times ',lapMins, lapSecs, lapTenths);
+  console.log('prevLap ', prevLap);
+  console.log('lapTimes ', lapTimes);
+
+  lapInfo.innerHTML = lapTimes.map((lapTime, index) => {
+    return `<li>
+              <span>Lap ${lapTimes.length - index}</span>
+              <span>${lapTime}</span>
+            </li>`;
   }).join('');
+
+  // let lapTime = `${mins}${secs}.${tenths}`;
+
+  // lapTimesClicked.unshift(lapTime);
+
+  // if (lapTimesClicked[1]) {
+  //   let prevLapTime = lapTimesClicked[1];
+  //   lapTimeConverted = (lapTime - prevLapTime).toFixed(2);
+
+  //   lapTimesConverted.unshift(lapTimeConverted);
+  // } else {
+  //   lapTimesConverted.push(parseFloat(lapTime));
+  // }
+
+  // lapInfo.innerHTML = lapTimesConverted.map(lapTime => {
+  //   if( lapTime == Math.min(...lapTimesConverted)) {
+  //     return `<li class="green white-text">${lapTime}</li>`;
+  //   } else if (lapTime == Math.max(...lapTimesConverted))  {
+  //     return `<li class="red white-text">${lapTime}</li>`;
+  //   } else {
+  //     return `<li>${lapTime}</li>`;
+  //   } 
+  // }).join('');
 }
 
 function stop() {
@@ -109,8 +154,7 @@ function reset() {
   tenthSpan.innerHTML = tenths;
   minSpan.innerHTML = mins;
   lapInfo.innerHTML = '';
-  lapTimesClicked = [];
-  lapTimesConverted = [];
+  prevLap
 }
 
 const util = {
